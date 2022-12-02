0a6b43ada5f592d2b7eec07b603585aa5d5633aa
Kimon Krenz <kimon.krenz@part9.com>
2022-12-02 19:09:50 +0000

    Create tidy_postcode.R

---
+++ tidy_postcode.R
@@ -0,0 +1,13 @@
+#' Altered code from NHSBSA Data Analytics https://github.com/nhsbsa-data-analytics/addressMatchR
+
+# function to tidy postcode data
+tidy_postcode <- function(df, col) {
+
+  # Tide the postcode column
+df <- df %>%
+  # Uppercase
+  dplyr::mutate(postcode = toupper({{ col }})) %>%
+  # Remove anything not a character or digit
+  dplyr::mutate(postcode = gsub("[^A-Z0-9]", "",{{ col }}, ignore.case = TRUE, perl = TRUE))
+
+}
