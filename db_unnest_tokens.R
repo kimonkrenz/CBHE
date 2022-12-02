1e0d52af5d96b3e7ddf61ed6715e23019f00588d
Kimon Krenz <kimon.krenz@part9.com>
2022-12-02 19:29:14 +0000

    Update db_unnest_tokens.R

--- db_unnest_tokens.R
+++ db_unnest_tokens.R
@@ -1,6 +1,7 @@
 #' Altered code from NHSBSA Data Analytics https://github.com/nhsbsa-data-analytics/nhsbsaR
 #'
 #' Utilising tidy's unnest_tokens
+
 db_unnest_tokens <- function(df, col, drop = TRUE, regex = "[:space:]") {
 
   df <- tidytext::unnest_tokens(df, "token", col, token = "regex", pattern = regex, format = "text", to_lower = FALSE, drop = TRUE, collapse = NULL) %>% dplyr::mutate(token_number = row.names(.))
