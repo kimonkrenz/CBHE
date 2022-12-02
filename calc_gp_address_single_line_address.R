b62ac8ecbb58a6caf85fd1b7fb1c0a138ef88217
Kimon Krenz <kimon.krenz@part9.com>
2022-12-02 19:06:50 +0000

    Create calc_gp_address_single_line_address.R

---
+++ calc_gp_address_single_line_address.R
@@ -0,0 +1,30 @@
+#' Altered code from NHSBSA Data Analytics https://github.com/nhsbsa-data-analytics/addressMatchR
+
+# function for GP addess data
+calc_gp_address_single_line_address <- function(
+  df,
+  include_postcode = FALSE
+) {
+
+  # Create the single line address
+  df <- dplyr::mutate(df,
+        single_line_address = paste0( 
+        ifelse(nameofbuilding == "", "", paste0(nameofbuilding, ", ") ),
+        ifelse(numberofbuilding == "", "", paste0(numberofbuilding, ", ") ),
+        ifelse(nameofroad == "", "", paste0(nameofroad, ", ") ),
+        ifelse(nameoflocality == "", "", paste0(nameoflocality, ", ") ),
+        ifelse(nameoftown == "", "", paste0(nameoftown, ", ") )
+        ))
+
+  # Add the postcode if necessary
+  if (include_postcode) {
+
+    df <- dplyr::mutate(df,
+        single_line_address = paste0(single_line_address, postcode)
+      )
+
+  }
+
+  df
+
+}
